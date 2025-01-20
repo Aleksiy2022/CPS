e1d8ec9cb5bb948d697b32cc7039ddbe390fa6cd
kashmir <kuznetcov.alekcey@icloud.com>
2025-01-20 19:44:30 +0300

    Mobile first ready.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.slider', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: 1.25
+  });
